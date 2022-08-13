3d0316c49a4b34bbf81d9ac45edba1169b973da7
vinceTheProgrammer <vincetheprogrammer@gmail.com>
2022-08-13 05:02:40 -0500

    Initial Commit

--- build/listeners/ready.d.ts
+++
@@ -1,6 +0,0 @@
-import { Listener } from '@sapphire/framework';
-import type { Client } from 'discord.js';
-export declare class ReadyListener extends Listener {
-    run(client: Client): void;
-}
-//# sourceMappingURL=ready.d.ts.map
